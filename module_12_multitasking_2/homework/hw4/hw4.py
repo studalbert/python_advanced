6d73c2da41baa9266beb4ccb77f045d15e08a6ba
user <albertik_1098@mail.ru>
2024-12-07 16:59:04 +0500

    add m12 homework, fix 12.4

--- module_12_multitasking_2/homework/hw4/hw4.py
+++ module_12_multitasking_2/homework/hw4/hw4.py
@@ -1,6 +1,6 @@
 import threading
 import time
-from queue import Queue, Empty
+from queue import PriorityQueue
 
 import requests
 
@@ -15,22 +15,23 @@ def func_for_threads(queue):
         if req.status_code != 200:
             continue
         data = req.text
-        queue.put(f"{timestamp} {data}")
+        queue.put((timestamp, data))
         time.sleep(1)
 
 
 if __name__ == "__main__":
-    queue = Queue()
+    queue = PriorityQueue()
+    threads = []
     for i in range(10):
         time.sleep(1)
         thread = threading.Thread(target=func_for_threads, args=(queue,))
         thread.start()
+        threads.append(thread)
+    for thread in threads:
+        thread.join()
 
     with open("hw4.log", "w") as file:
-        while True:
-            try:
-                log = queue.get(timeout=1.2)
-                queue.task_done()
-                file.write(log + "\n")
-            except Empty:
-                break
+        while not queue.empty():
+            priority, data = queue.get()
+            queue.task_done()
+            file.write(f"{priority} {data}" + "\n")
