b9a48a5f02aed706156c08096d93f89fdb34ab35
user <albertik_1098@mail.ru>
2024-12-25 00:28:37 +0500

    add m17 homework

--- module_17_rest_api/homework/app/models.py
+++ module_17_rest_api/homework/app/models.py
@@ -3,19 +3,43 @@ from dataclasses import dataclass
 from typing import Optional, Union, List, Dict
 
 DATA = [
-    {'id': 0, 'title': 'A Byte of Python', 'author': 'Swaroop C. H.'},
-    {'id': 1, 'title': 'Moby-Dick; or, The Whale', 'author': 'Herman Melville'},
-    {'id': 3, 'title': 'War and Peace', 'author': 'Leo Tolstoy'},
+    {
+        "id": 0,
+        "title": "A Byte of Python",
+        "author": {"id": 1, "first_name": "Swaroop", "last_name": "Chiltur"},
+    },
+    {
+        "id": 1,
+        "title": "Moby-Dick; or, The Whale",
+        "author": {"id": 2, "first_name": "Herman", "last_name": "Melville"},
+    },
+    {
+        "id": 2,
+        "title": "War and Peace",
+        "author": {"id": 3, "first_name": "Leo", "last_name": "Tolstoy"},
+    },
 ]
 
-DATABASE_NAME = 'table_books.db'
-BOOKS_TABLE_NAME = 'books'
+DATABASE_NAME = "table_books.db"
+BOOKS_TABLE_NAME = "books"
+AUTHORS_TABLE_NAME = "authors"
+ENABLE_FOREIGN_KEY = "PRAGMA foreign_keys = ON;"
+
+
+@dataclass
+class Author:
+    first_name: str
+    last_name: str
+    id: Optional[int] = None
+
+    def __getitem__(self, item: str) -> Union[int, str]:
+        return getattr(self, item)
 
 
 @dataclass
 class Book:
     title: str
-    author: str
+    author: Author
     id: Optional[int] = None
 
     def __getitem__(self, item: str) -> Union[int, str]:
@@ -25,6 +49,34 @@ class Book:
 def init_db(initial_records: List[Dict]) -> None:
     with sqlite3.connect(DATABASE_NAME) as conn:
         cursor = conn.cursor()
+        cursor.execute("PRAGMA foreign_keys = ON")
+        cursor.execute(
+            f"""
+                    SELECT name FROM sqlite_master
+                    WHERE type='table' AND name='{AUTHORS_TABLE_NAME}';
+                    """
+        )
+        exists = cursor.fetchone()
+        if not exists:
+            cursor.executescript(
+                f"""
+                                    CREATE TABLE `{AUTHORS_TABLE_NAME}`(
+                                        id INTEGER PRIMARY KEY AUTOINCREMENT, 
+                                        first_name TEXT,
+                                        last_name TEXT
+                                    );
+                                    """
+            )
+            cursor.executemany(
+                f"""
+                            INSERT INTO `{AUTHORS_TABLE_NAME}`
+                            (first_name, last_name) VALUES (?, ?)
+                            """,
+                [
+                    (item["author"]["first_name"], item["author"]["last_name"])
+                    for item in initial_records
+                ],
+            )
         cursor.execute(
             f"""
             SELECT name FROM sqlite_master
@@ -38,7 +90,7 @@ def init_db(initial_records: List[Dict]) -> None:
                 CREATE TABLE `{BOOKS_TABLE_NAME}`(
                     id INTEGER PRIMARY KEY AUTOINCREMENT, 
                     title TEXT,
-                    author TEXT
+                    author integer not null references authors (id) on delete cascade 
                 );
                 """
             )
@@ -47,21 +99,24 @@ def init_db(initial_records: List[Dict]) -> None:
                 INSERT INTO `{BOOKS_TABLE_NAME}`
                 (title, author) VALUES (?, ?)
                 """,
-                [
-                    (item['title'], item['author'])
-                    for item in initial_records
-                ]
+                [(item["title"], item["author"]["id"]) for item in initial_records],
             )
 
 
 def _get_book_obj_from_row(row: tuple) -> Book:
-    return Book(id=row[0], title=row[1], author=row[2])
+    return Book(
+        id=row[0],
+        title=row[1],
+        author=Author(id=row[3], first_name=row[4], last_name=row[5]),
+    )
 
 
 def get_all_books() -> list[Book]:
     with sqlite3.connect(DATABASE_NAME) as conn:
         cursor = conn.cursor()
-        cursor.execute(f'SELECT * FROM `{BOOKS_TABLE_NAME}`')
+        cursor.execute(
+            f"SELECT * FROM `{BOOKS_TABLE_NAME}` join {AUTHORS_TABLE_NAME} on {BOOKS_TABLE_NAME}.author = {AUTHORS_TABLE_NAME}.id"
+        )
         all_books = cursor.fetchall()
         return [_get_book_obj_from_row(row) for row in all_books]
 
@@ -70,11 +125,27 @@ def add_book(book: Book) -> Book:
     with sqlite3.connect(DATABASE_NAME) as conn:
         cursor = conn.cursor()
         cursor.execute(
+            f"""select * from {AUTHORS_TABLE_NAME} where first_name = ? and last_name = ?""",
+            (book.author.first_name, book.author.last_name),
+        )
+        res = cursor.fetchone()
+        if not res:
+            cursor.execute(
+                f"""
+                        INSERT INTO `{AUTHORS_TABLE_NAME}` 
+                        (first_name, last_name) VALUES (?, ?)
+                        """,
+                (book.author.first_name, book.author.last_name),
+            )
+            book.author.id = cursor.lastrowid
+        else:
+            book.author.id = res[0]
+        cursor.execute(
             f"""
             INSERT INTO `{BOOKS_TABLE_NAME}` 
             (title, author) VALUES (?, ?)
             """,
-            (book.title, book.author)
+            (book.title, book.author.id),
         )
         book.id = cursor.lastrowid
         return book
@@ -85,9 +156,11 @@ def get_book_by_id(book_id: int) -> Optional[Book]:
         cursor = conn.cursor()
         cursor.execute(
             f"""
-            SELECT * FROM `{BOOKS_TABLE_NAME}` WHERE id = ?
+            SELECT * FROM `{BOOKS_TABLE_NAME}` 
+            join {AUTHORS_TABLE_NAME} on {BOOKS_TABLE_NAME}.author = {AUTHORS_TABLE_NAME}.id 
+            WHERE {BOOKS_TABLE_NAME}.id = ?
             """,
-            (book_id,)
+            (book_id,),
         )
         book = cursor.fetchone()
         if book:
@@ -99,12 +172,25 @@ def update_book_by_id(book: Book) -> None:
         cursor = conn.cursor()
         cursor.execute(
             f"""
-            UPDATE {BOOKS_TABLE_NAME}
-            SET title = ?, author = ?
-            WHERE id = ?
-            """,
-            (book.title, book.author, book.id)
+                    UPDATE {BOOKS_TABLE_NAME}
+                    SET title = ?
+                    WHERE id = ?
+                    """,
+            (book.title, book.id),
         )
+        book.author.id = cursor.execute(
+            f"""SELECT author from {BOOKS_TABLE_NAME} where id = ?""", (book.id,)
+        ).fetchone()[0]
+        # Обновляем информацию об авторе
+        cursor.execute(
+            f"""
+                    UPDATE {AUTHORS_TABLE_NAME}
+                    SET first_name = ?, last_name = ?
+                    WHERE id = ?
+                    """,
+            (book.author.first_name, book.author.last_name, book.author.id),
+        )
+
         conn.commit()
 
 
@@ -116,9 +202,13 @@ def delete_book_by_id(book_id: int) -> None:
             DELETE FROM {BOOKS_TABLE_NAME}
             WHERE id = ?
             """,
-            (book_id,)
+            (book_id,),
         )
         conn.commit()
+        if cursor.rowcount == 0:
+            raise ValueError(
+                "No book found with the given ID"
+            )  # Генерируем исключение, если книга не найдена
 
 
 def get_book_by_title(book_title: str) -> Optional[Book]:
@@ -126,10 +216,96 @@ def get_book_by_title(book_title: str) -> Optional[Book]:
         cursor = conn.cursor()
         cursor.execute(
             f"""
-            SELECT * FROM `{BOOKS_TABLE_NAME}` WHERE title = ?
+            SELECT * FROM `{BOOKS_TABLE_NAME}` join {AUTHORS_TABLE_NAME} on {BOOKS_TABLE_NAME}.author = {BOOKS_TABLE_NAME}.id WHERE title = ?
             """,
-            (book_title,)
+            (book_title,),
         )
         book = cursor.fetchone()
         if book:
             return _get_book_obj_from_row(book)
+
+
+def _get_author_obj_from_row(row):
+    return Author(id=row[0], first_name=row[1], last_name=row[2])
+
+
+def get_author_by_id(author_id):
+    with sqlite3.connect(DATABASE_NAME) as conn:
+        cursor = conn.cursor()
+        cursor.execute(
+            f"""
+        select * from {AUTHORS_TABLE_NAME} where id = ?""",
+            (author_id,),
+        )
+        author = cursor.fetchone()
+        if author:
+            return _get_author_obj_from_row(author)
+
+
+def get_author_by_name(first_name, last_name):
+    with sqlite3.connect(DATABASE_NAME) as conn:
+        cursor = conn.cursor()
+        cursor.execute(
+            f"""
+        select * from {AUTHORS_TABLE_NAME} where first_name = ? and last_name = ?""",
+            (first_name, last_name),
+        )
+        author = cursor.fetchone()
+        if author:
+            return _get_author_obj_from_row(author)
+
+
+def add_author(author: Author) -> Author:
+    with sqlite3.connect(DATABASE_NAME) as conn:
+        cursor = conn.cursor()
+        cursor.execute(
+            f"""select * from {AUTHORS_TABLE_NAME} where first_name = ? and last_name = ?""",
+            (author.first_name, author.last_name),
+        )
+        res = cursor.fetchone()
+        if not res:
+            cursor.execute(
+                f"""
+                INSERT INTO `{AUTHORS_TABLE_NAME}` 
+                (first_name, last_name) VALUES (?, ?)
+                """,
+                (author.first_name, author.last_name),
+            )
+            author.id = cursor.lastrowid
+            return author
+        else:
+            author.id = res[0]
+            return author
+
+
+def get_books_by_author(author_id) -> list[Book]:
+    with sqlite3.connect(DATABASE_NAME) as conn:
+        cursor = conn.cursor()
+        cursor.execute(
+            f"SELECT * from {BOOKS_TABLE_NAME} "
+            f"join {AUTHORS_TABLE_NAME} on {BOOKS_TABLE_NAME}.author={AUTHORS_TABLE_NAME}.id  "
+            f"where {AUTHORS_TABLE_NAME}.id = ?",
+            (author_id,),
+        )
+        all_books = cursor.fetchall()
+        if all_books:
+            return [_get_book_obj_from_row(row) for row in all_books]
+        else:
+            raise ValueError("No books found for the given author ID.")
+
+
+def delete_author_by_id(author_id):
+    with sqlite3.connect(DATABASE_NAME) as conn:
+        cursor = conn.cursor()
+        cursor.execute(
+            f"""
+            DELETE FROM {AUTHORS_TABLE_NAME}
+            WHERE id = ?
+            """,
+            (author_id,),
+        )
+        conn.commit()
+        if cursor.rowcount == 0:
+            raise ValueError(
+                "No author found with the given ID"
+            )  # Генерируем исключение, если автор не найден
--- module_17_rest_api/homework/app/routes.py
+++ module_17_rest_api/homework/app/routes.py
@@ -8,7 +8,15 @@ from models import (
     init_db,
     add_book,
 )
-from schemas import BookSchema
+from module_17_rest_api.homework.app.models import (
+    get_book_by_id,
+    update_book_by_id,
+    delete_book_by_id,
+    get_books_by_author,
+    add_author,
+    delete_author_by_id,
+)
+from schemas import BookSchema, AuthorSchema
 
 app = Flask(__name__)
 api = Api(app)
@@ -31,8 +39,65 @@ class BookList(Resource):
         return schema.dump(book), 201
 
 
-api.add_resource(BookList, '/api/books')
+class Books(Resource):
+    def get(self, id: int):
+        book = get_book_by_id(id)
+        if book:
+            schema = BookSchema()
+            return schema.dump(book)
+        else:
+            return {"data": "Book not found"}, 404
 
-if __name__ == '__main__':
+    def put(self, id):
+        data = request.json
+        schema = BookSchema()
+        try:
+            book = schema.load(data)
+            book.id = id
+        except ValidationError as exc:
+            return exc.messages, 400
+        update_book_by_id(book)
+        return schema.dump(get_book_by_id(id)), 200
+
+    def delete(self, id):
+        try:
+            delete_book_by_id(id)
+            return {"data": "Book deleted succesfully"}, 204
+        except ValueError:
+            return {"data": "Resource not found."}, 404
+
+
+class Authors(Resource):
+    def post(self):
+        data = request.json
+        schema = AuthorSchema()
+        try:
+            author = schema.load(data)
+        except ValidationError as exc:
+            return exc.messages, 400
+        author = add_author(author)
+        return schema.dump(author), 201
+
+    def get(self, id):
+        schema = BookSchema()
+        try:
+            books = get_books_by_author(id)
+            return schema.dump(books, many=True), 200
+        except ValueError:
+            return {"data": "Resource not found."}, 404
+
+    def delete(self, id):
+        try:
+            delete_author_by_id(id)
+            return {"data": "the author was deleted succesfully"}, 204
+        except ValueError:
+            return {"data": "Resource not found."}, 404
+
+
+api.add_resource(BookList, "/api/books")
+api.add_resource(Books, "/api/books/<int:id>")
+api.add_resource(Authors, "/api/authors", "/api/authors/<int:id>")
+
+if __name__ == "__main__":
     init_db(initial_records=DATA)
     app.run(debug=True)
