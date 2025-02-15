1526836130f1317cafd44bf7119a9387dd011a47
user <albertik_1098@mail.ru>
2025-02-15 19:44:27 +0500

    add m28 homework

--- module_28_security/homework/task_2/hw.py
+++ module_28_security/homework/task_2/hw.py
@@ -1,12 +1,32 @@
+from flask import Flask, render_template_string, request, Response
+
+app = Flask(__name__)
+
 HTML = """
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <title>Title</title>
+   <meta charset="UTF-8">
+   <title>Title</title>
 </head>
 <body>
-  {user_input}
+ {{ user_input | safe }}
 </body>
 </html>
 """
+
+
+@app.route("/")
+def index():
+    user_input = request.args.get("user_input", "")
+    return render_template_string(HTML, user_input=user_input)
+
+
+@app.after_request
+def apply_csp(response: Response):
+    response.headers["Content-Security-Policy"] = "script-src 'self'"
+    return response
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
